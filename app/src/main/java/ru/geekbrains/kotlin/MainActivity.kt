90ba4f56f1c2648fcd519a6a25a04ee531d30785
mari_leva <savosh999@yandex.ru>
2022-03-24 22:21:06 +0700

    Урок 1. Добавила кнопку и повесила на нее clickListener

--- app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.geekbrains.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                findViewById<TextView>(R.id.textView).text = "Нажата кнопка!"
+            }
+        })
+
+
     }
 }
