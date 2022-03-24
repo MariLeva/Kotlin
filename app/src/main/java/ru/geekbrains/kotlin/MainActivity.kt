5db09f9034232b7021c4374cef3e192efcc41833
mari_leva <savosh999@yandex.ru>
2022-03-24 22:54:25 +0700

    Урок 1. Добавила data class Note с тремя свойствами и вывела на экран, так же добавила и вывела копию

--- app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val noteOne = Note("Утро ", "Прогулка с собакой ", Date())
+        val noteTwo = noteOne.copy()
 
         findViewById<Button>(R.id.button).setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                findViewById<TextView>(R.id.textView).text = "Нажата кнопка!"
+                findViewById<TextView>(R.id.textViewOne).text = noteOne.title + noteOne.note + noteOne.data
+                findViewById<TextView>(R.id.textViewTwo).text = noteTwo.title + noteTwo.note + noteTwo.data
             }
         })
 
---
+++ app/src/main/java/ru/geekbrains/kotlin/Note.kt
@@ -0,0 +1,6 @@
+package ru.geekbrains.kotlin
+
+import java.util.*
+
+data class Note(val title: String, val note: String, val data: Date)
+
