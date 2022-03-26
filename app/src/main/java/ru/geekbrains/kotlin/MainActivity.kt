dc79c922f587cd2be79b950c17f0e75c2088258e
mari_leva <savosh999@yandex.ru>
2022-03-27 01:04:58 +0700

    Урок 1. Добавила циклы

--- app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.geekbrains.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -11,17 +12,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val noteOne = Note("Утро ", "Прогулка с собакой ", Date())
+
+        val noteOne = Note("Утро", "Прогулка с собакой", Date())
         val noteTwo = noteOne.copy()
 
         findViewById<Button>(R.id.button).setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                findViewById<TextView>(R.id.textViewOne).text = noteOne.title + noteOne.note + noteOne.data
-                findViewById<TextView>(R.id.textViewTwo).text = noteTwo.title + noteTwo.note + noteTwo.data
+                findViewById<TextView>(R.id.textViewOne).text = noteOne.title + " " + noteOne.note + " " + noteOne.data
+                findViewById<TextView>(R.id.textViewTwo).text = noteTwo.title + " " + noteTwo.note + " " + noteTwo.data
             }
         })
 
-
+        val text = (if (noteOne.title.equals("Утро")) noteOne.note else "Нет дел!")
+        Log.d("myLog","${text.toString()}")
+        Log.d("myLog","${DataBase.getWhen(DayEnum.MORNING)}")
+        DataBase.getFor()
     }
 }
--- app/src/main/java/ru/geekbrains/kotlin/Note.kt
+++ app/src/main/java/ru/geekbrains/kotlin/Note.kt
@@ -1,6 +1,30 @@
 package ru.geekbrains.kotlin
 
+import android.util.Log
 import java.util.*
 
 data class Note(val title: String, val note: String, val data: Date)
 
+enum class DayEnum {
+    MORNING,
+    DAY,
+    EVENING
+}
+
+object DataBase {
+    fun getWhen(input: DayEnum): String {
+        return when (input) {
+            DayEnum.MORNING -> "Утро"
+            DayEnum.DAY -> "День"
+            DayEnum.EVENING -> "Вечер"
+        }
+    }
+
+    fun getFor() {
+        val listDay = listOf("Утро", "День", "Вечер")
+        for (day in listDay) {
+            Log.d("myLog","${day}")
+        }
+    }
+
+}
