e6b3292b836b03d3d2d967f8198730dd9c619bec
mari_leva <savosh999@yandex.ru>
2022-03-30 11:31:51 +0700

    Урок 2. Добавила Binding

--- app/src/main/java/ru/geekbrains/kotlin/view/main/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/kotlin/view/main/MainFragment.kt
@@ -18,6 +18,7 @@ private const val ARG_PARAM2 = "param2"
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
+    lateinit var binding: FragmentMainBinding
 
     companion object {
         @JvmStatic
@@ -27,11 +28,15 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        //return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tvName.text = "Binding GOOD!"
+
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         val observer = object : Observer<Any> {
             override fun onChanged(data: Any) {
@@ -40,6 +45,11 @@ class MainFragment : Fragment() {
         }
         viewModel.getData().observe(viewLifecycleOwner, observer)
         viewModel.getWeather()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+       // binding = null
     }
 
     private fun renderData(data: Any) {
