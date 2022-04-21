97bc5ff252d5816c0eb7fc69803c289c65edec3f
mari_leva <savosh999@yandex.ru>
2022-04-21 13:02:03 +0700

    Урок 6. Подписалась на события изменения связи и авиарежим

--- app/src/main/java/ru/geekbrains/kotlin/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin/view/MainActivity.kt
@@ -1,17 +1,27 @@
 package ru.geekbrains.kotlin.view
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.geekbrains.kotlin.R
 import ru.geekbrains.kotlin.databinding.ActivityMainBinding
+import ru.geekbrains.kotlin.repository.MainBroadcastReceiver
 import ru.geekbrains.kotlin.view.weatherList.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val receiver = MainBroadcastReceiver()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (savedInstanceState == null) {
